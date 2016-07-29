9b8b4dfc23d6469f011364f9bedc614a6ab67615
midhruvjaink <blivefoundations@gmail.com>
2016-07-29 21:52:46 +0530

    codechef

---
+++ C/pooja.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main(){
+int x; float y;
+scanf("%d", &x);
+scanf("%f", &y);
+if(x>0&&x<=2000&&y>=0&&y<=2000)
+{
+	if(y>=x+0.50)
+	{
+		if(x%5==0)
+		{
+    		y=y-x-0.50;
+    		printf("the total amount left= %.2f", y);
+		}
+		else
+		{
+			printf("x is not multiple of 5");
+			printf("the total amount left= %.2f", y);
+		}
+	}
+	else{
+		printf("You can not take out!");
+	}
+
+}
+else{
+	printf("enter value bw 0-2000\n");
+	}
+	return 0;
+
+	}
+	
---
+++ C/set.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+void main(){
+int x, y;
+scanf("%d %d",&x, &y);
+printf("Sum=%d", x+y);
+}
