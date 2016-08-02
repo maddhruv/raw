f8e2b65954e3af5060682ef7ec78df0d936b9bd3
midhruvjaink <blivefoundations@gmail.com>
2016-08-02 11:16:41 +0530

    new

--- python/cuberoot.py
+++ python/cuberoot.py
@@ -1,8 +1,8 @@
 x = int(input('Enter the number:'))
 ans = 0
-while ans*ans*ans < abs(x):
+while ans**3 < abs(x):
     ans += 1
-if ans*ans*ans != abs(x):
+if ans*83 != abs(x):
     print('x is not a perfect cube')
 else:
     if x < 0:
---
+++ python/math.py
@@ -0,0 +1,4 @@
+import math
+r=10
+d=2*math.pi*r
+print(d)
