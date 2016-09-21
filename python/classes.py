b2db3bb9bd99985d3c70755468ef376bce6e067b
midhruvjaink <blivefoundations@gmail.com>
2016-09-21 15:20:00 +0100

    classes programs

---
+++ python/classes.py
@@ -0,0 +1,10 @@
+class Hello:
+	def say(self):
+		print("Hello")
+	def louder(self):
+		print("HELLLLOO")
+
+ob = Hello()
+
+ob.say()
+ob.louder()
---
+++ python/fruit_classes.py
@@ -0,0 +1,19 @@
+class Fruit:
+	no = 12
+	def eat(self):
+		print("khaaaouch!!")
+		self.no -=1
+
+	def show(self):
+		print(str(self.no) + " left")
+
+apple = Fruit()
+mango = Fruit()
+
+apple.eat(	)
+apple.eat( )
+apple.show()
+mango.eat()
+mango.show()
+mango.eat()
+mango.show()
