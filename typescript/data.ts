c83bbce49686b9322bacfcd618f51bfede985ee1
midhruvjaink <blivefoundations@gmail.com>
2017-04-24 14:25:00 +0100

    typescript data

---
+++ typescript/data.ts
@@ -0,0 +1,17 @@
+var flag:boolean = true;
+
+var no:number = 12;
+
+var str:string = "Hello";
+
+var arr:number[] = [1, 2, 3];
+
+var anyArr:any[] = [1, "hello"];
+
+
+enum constants{
+  pi = 3.14,
+  e = 2.7
+}
+
+console.log(2*constants.pi*no)
